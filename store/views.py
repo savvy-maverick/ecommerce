ad7aa702e7a717789d09bd11222ab0cc7dd604dd
savvy-maverick <160122543+savvy-maverick@users.noreply.github.com>
2024-11-08 06:20:31 -0800

    commit 2

---
+++ store/views.py
@@ -0,0 +1,169 @@
+from django.shortcuts import render, redirect
+from .models import Category, Product, Customer, Order,Profile
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm,UserInfoForm
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+from django import forms
+from django.db.models import Q
+import json
+from cart.cart import Cart
+
+# Create your views here.
+
+def search(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+        if not searched:
+            messages.success(request, ("that product does not exist"))
+            return render(request, 'search.html')
+        else:
+            return render(request, "search.html", {'searched': searched})
+    else:
+        return render(request, "search.html")
+    
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        # get current user
+        current_user = Profile.objects.get(user__id=request.user.id)
+        # get current user shipping info
+        shipping_user = ShippingAddress.objects.get(id=request.user.id)
+        # get original user form
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        # get user shipping form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        
+        if form.is_valid() or shipping_form.is_valid():
+            form.save()
+            shipping_form.save()
+            
+        
+            messages.success(request, ("Your info has been updated!!"))
+            return redirect('home')
+        return render(request, 'update_info.html', {'form': form, 'shipping_form':shipping_form} )
+    else:
+        messages.success(request, ("You must be logged in to access that page"))  
+        return redirect('home')  
+        # return render(request, 'update_info.html')
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "your password has been updated")
+                # login(request, current_user)
+                return redirect('login')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+        
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {'form': form})
+        
+    return render(request, 'update_password.html')
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+        
+        if user_form.is_valid():
+            user_form.save()
+            
+            login(request, current_user)
+            messages.success(request, ("User has been updated!!"))
+            return redirect('home')
+        return render(request, 'update_user.html', {'user_form': user_form} )
+    else:
+        messages.success(request, ("You must be logged in to access that page"))  
+        return redirect('home')  
+
+
+def category_summary(request):
+    categories = Category.objects.all()
+
+    return render(request, 'category_summary.html', {'categories': categories})
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password )
+        if user is not None:
+            login(request, user)
+            # do some shopping cart stuff
+            current_user = Profile.objects.get(user__id=request.user.id)
+            # get their saved cart
+            saved_cart = current_user.old_cart
+            # convert database string to python dictionary
+            if saved_cart:
+                # convert to dictionary using json
+                converted_cart= json.loads(saved_cart)
+                # add the loaded cart dictionary to session
+                # get the cart
+                cart = Cart(request)
+                # LOOP THRU THE CART AND ADD PRODUCT FROM DATABASE
+                for key,value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
+            messages.success(request, ("you have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request,("there was an error"))
+            return redirect('login') 
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have logged out"))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form =  SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You've registered successfully"))
+            return redirect('update_info')
+        else:
+            messages.success(request, ("oops, there was an error"))
+    return render(request, 'register.html', {'form': form})
+    
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category  })
+    except:
+       messages.success(request, ('that category does not exist')) 
+       return redirect('home')
+   
